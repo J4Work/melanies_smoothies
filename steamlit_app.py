f37d2c18e9dbb0e3f0704704b73caf639e3ede99
J4Work <152456315+J4Work@users.noreply.github.com>
2024-10-21 15:39:05 +1100

    Update steamlit_app.py
    add Requests

--- steamlit_app.py
+++ steamlit_app.py
@@ -52,3 +52,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
