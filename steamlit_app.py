9391dd239d0b4930e8bfdfa8ede3a48b0f011cc7
J4Work <152456315+J4Work@users.noreply.github.com>
2024-10-21 16:58:28 +1100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -57,6 +57,8 @@ if ingredients_list:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
+# returning response [502], no idea what that means but it's just error after error from this point onward!
+
 #fv_df = st.dataframe(data=fruityvice_response.json(), use_conatiner_width=True)
 
 
