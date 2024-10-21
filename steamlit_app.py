2e6d87f19c1e0e3dd82b73860ce97e44d1ce69d4
J4Work <152456315+J4Work@users.noreply.github.com>
2024-10-21 16:50:26 +1100

    Update steamlit_app.py
    retrace previous steps

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-import requests
+
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -39,9 +39,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_conatiner_width=True)
+        #st.subheader(fruit_chosen + ' Nutrition Information')
+
     
 
     # my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -55,7 +54,9 @@ if ingredients_list:
     #     session.sql(my_insert_stmt).collect()
     #     st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-
-
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_conatiner_width=True)
 
 
